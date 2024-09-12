6589cff228a14580c7369448c1e2a57e081a343a
Owen Rodriguez <owenzeledon@icloud.com>
2024-09-12 19:12:43 -0400

    feat: metadata has been added

---
+++ app/MetaData.jsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+
+const MetaData = () => {
+  return (
+    <Head>
+      <title>Owen R - Developer</title>
+      <meta
+        name="description"
+        content="I'm happy to present you my personal portfolio created by me. I'm a software developer and designer open to work on anything you need, from landing pages to a full ecommerce website with an admin panel."
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="UTF-8" />
+      <meta property="og:title" content="Owen R - Developer" />
+      <meta
+        property="og:description"
+        content="I'm happy to present you my personal portfolio created by me. I'm a software developer and designer open to work on anything you need, from landing pages to a full ecommerce website with an admin panel."
+      />
+      <meta property="og:url" content="https://owendev-one.vercel.app" />
+      <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:title" content="Owen R - Developer" />
+      <meta
+        property="twitter:description"
+        content="I'm happy to present you my personal portfolio created by me. I'm a software developer and designer open to work on anything you need, from landing pages to a full ecommerce website with an admin panel."
+      />
+      <link rel="canonical" href="https://owendev-one.vercel.app" />
+      <meta
+        name="keywords"
+        content="web, web developer, software, software developer, ecommerce, admin panel, portfolio, developer, designer, web designer"
+      />
+    </Head>
+  );
+};
+
+export default MetaData;
